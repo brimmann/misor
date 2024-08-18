630232d5dca5270627239bcfe33b3553c6cf2aee
Rashid <rashidstanikzai.net@gmail.com>
2024-08-18 20:01:13 +0500

    chore: manage.py moved outside
    temp

--- manage.py
+++ manage.py
@@ -6,7 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'misor.settings')
+
+    # manage.py location
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, 'src'))
+
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_package.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
